efee55de9085eefb57c0c2b38bdcddc1c7404f30
Your Name <you@example.com>
2023-03-15 21:19:47 +0200

    -15.03.2023

---
+++ src/test/java/TestDB.java
@@ -0,0 +1,30 @@
+import dataobjects.AdressTable;
+import mysqlconfig.StatemenT;
+import mysqlconfig.enums.ADRESSFIELDS;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+public class TestDB {
+    static StatemenT statemenT;
+    @BeforeClass
+    public static void b(){
+        statemenT=new StatemenT();
+    }
+    @Test
+    public void test1() throws SQLException {
+       statemenT.selectByFieldName(ADRESSFIELDS.NAME,"Vasiljjjdy");
+    }
+    @Test
+    public void test2() throws SQLException {
+        Assert.assertNotNull(statemenT.selectByFieldName(ADRESSFIELDS.NAME,"Elena").email);
+    }
+    @Test
+    public void test3() throws SQLException {
+        for (AdressTable l:statemenT.selectListTablesMail(ADRESSFIELDS.EMAIL,"hhh.com")){
+            System.out.println(l);
+        }
+    }
+}
